d48b0b99e846665ecd1fe48d89b8d18e800eabd8
Samuel Issadore <94148426+theutisticcoder@users.noreply.github.com>
2026-09-23 01:56:19 +0000

    fix code

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
         socket.join(n[1]);
         socket.emit("player", users[sockets.length]);
     })
-    io.on("start", rm => {
+    socket.on("start", rm => {
         console.log(rm);
         io.to(rm).emit("text1");
         rooms[rooms.findIndex(r => r.name === rm)].content.push([])
